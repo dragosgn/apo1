b08bd8aa1b1d8e2425575ff589f1086073bc35d3
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2019-06-01 16:30:47 +0200

    separating keys

--- src/StreamApp.js
+++ src/StreamApp.js
@@ -11,20 +11,29 @@ import {
 } from "react-activity-feed";
 import "react-activity-feed/dist/index.css";
 
+const {
+  REACT_APP_API_KEY,
+  REACT_APP_APP_ID,
+  REACT_APP_TOKEN,
+  REACT_APP_USER_ID
+} = process.env;
+
 export default class Stream extends React.Component {
   render() {
-    console.log("process.env", process.env);
+    console.log(
+      REACT_APP_API_KEY,
+      REACT_APP_APP_ID,
+      REACT_APP_TOKEN,
+      REACT_APP_USER_ID
+    );
     return (
       <StreamApp
-        apiKey="9nktx8a34w6u"
-        appId="53032"
-        token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoidXNlci1vbmUifQ.t3zgKqFUYR-CYzdL5jyA6ITl3xu8HZ-gT7FgnyAWR3s"
+        apiKey={REACT_APP_API_KEY}
+        appId={REACT_APP_APP_ID}
+        token={REACT_APP_TOKEN}
       >
         <NotificationDropdown notify />
-        <StatusUpdateForm
-          feedGroup="timeline"
-          userId="6b8f4df0-95a1-4ef9-b870-30c5747db58e"
-        />
+        <StatusUpdateForm feedGroup="timeline" userId={REACT_APP_USER_ID} />
         <FlatFeed
           notify
           Activity={props => (
