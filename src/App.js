7ed98ad686aacc4911edb17311241d8fee507f7b
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2019-06-01 12:04:54 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  StreamApp,
+  NotificationDropdown,
+  FlatFeed,
+  Activity,
+  LikeButton
+} from "react-activity-feed";
+import "react-activity-feed/dist/index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <StreamApp
+        apiKey="du8he7epvp94"
+        appId="45206"
+        token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiNmI4ZjRkZjAtOTVhMS00ZWY5LWI4NzAtMzBjNTc0N2RiNThlIn0.XbVHgFN9NcVBiL5sg878hFNne1RqiZTOmd2459lC41s"
+      >
+        <NotificationDropdown notify />
+        <FlatFeed
+          notify
+          Activity={props => (
+            <Activity
+              {...props}
+              Footer={() => (
+                <div style={{ padding: "8px 16px" }}>
+                  <LikeButton {...props} />
+                </div>
+              )}
+            />
+          )}
+        />
+      </StreamApp>
+    );
+  }
 }
 
 export default App;
