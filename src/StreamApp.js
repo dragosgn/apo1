2257a05bc9ec6a8320f6ce2c156acc6d4266a24f
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2019-06-01 17:47:55 +0200

    fixes

--- src/StreamApp.js
+++ src/StreamApp.js
@@ -11,29 +11,19 @@ import {
 } from "react-activity-feed";
 import "react-activity-feed/dist/index.css";
 
-const {
-  REACT_APP_API_KEY,
-  REACT_APP_APP_ID,
-  REACT_APP_TOKEN,
-  REACT_APP_USER_ID
-} = process.env;
-
 export default class Stream extends React.Component {
   render() {
-    console.log(
-      REACT_APP_API_KEY,
-      REACT_APP_APP_ID,
-      REACT_APP_TOKEN,
-      REACT_APP_USER_ID
-    );
     return (
       <StreamApp
-        apiKey={REACT_APP_API_KEY}
-        appId={REACT_APP_APP_ID}
-        token={REACT_APP_TOKEN}
+        apiKey={process.env.REACT_APP_API_KEY}
+        appId={process.env.REACT_APP_APP_ID}
+        token={process.env.REACT_APP_TOKEN}
       >
         <NotificationDropdown notify />
-        <StatusUpdateForm feedGroup="timeline" userId={REACT_APP_USER_ID} />
+        <StatusUpdateForm
+          feedGroup="timeline"
+          userId={process.env.REACT_APP_USER_ID}
+        />
         <FlatFeed
           notify
           Activity={props => (
