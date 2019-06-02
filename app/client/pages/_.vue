d84600d406b03164519902468267ddcb4385af93
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2019-06-02 12:09:13 +0200

    fixing badgets

--- app/client/pages/_.vue
+++ app/client/pages/_.vue
@@ -27,9 +27,55 @@
             </v-flex>
             <p></p>
           </v-card-text>
+          <template>
+  <div class="text-xs-center">
+    <v-badge
+      color="purple"
+      left
+      overlap
+    >
+      <template v-slot:badge>
+        <v-icon
+          dark
+          small
+        >
+          done
+        </v-icon>
+      </template>
+      <v-icon
+        color="grey lighten-1"
+        large
+      >
+        account_circle
+      </v-icon>
+    </v-badge>
+
+    <v-badge
+      overlap
+      color="orange"
+    >
+      <template v-slot:badge>
+        <v-icon
+          dark
+          small
+        >
+          notifications
+        </v-icon>
+      </template>
+      <v-icon
+        large
+        color="grey darken-1"
+      >
+        account_box
+      </v-icon>
+    </v-badge>
+  </div>
+</template>
+          
 
           <v-card-actions>
             <p></p>
+            
 
             <v-layout ml-3 pt-3 row justify-start fill-height column>
               <v-flex>Contact:</v-flex>
